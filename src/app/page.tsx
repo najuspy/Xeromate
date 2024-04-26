acef823b98e51d681a63406bf716e6e84386502f
Sujan Pandey <sazeal.sujan@gmail.com>
2024-04-26 17:09:53 +1000

    deleted source props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,33 +57,7 @@ const cardData: CardProps[] = [
   },
 ]
 
-const userSalesData: SalesProps[] = [
-  {
-    name: "Olivia Martin",
-    email: "olivia.martin@email.com",
-    saleAmount: "+$1,999.00"
-  },
-  {
-    name: "Jackson Lee",
-    email: "isabella.nguyen@email.com",
-    saleAmount: "+$1,999.00"
-  },
-  {
-    name: "Isabella Nguyen",
-    email: "isabella.nguyen@email.com",
-    saleAmount: "+$39.00"
-  },
-  {
-    name: "William Kim",
-    email: "will@email.com",
-    saleAmount: "+$299.00"
-  },
-  {
-    name: "Sofia Davis",
-    email: "sofia.davis@email.com",
-    saleAmount: "+$39.00"
-  }
-];
+
 const companyData = [
   {
     name: 'Qantum Eight',
